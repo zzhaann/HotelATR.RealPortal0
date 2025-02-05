683b800562c33017e5264b3244c990077a6ef1b4
zzhaann <zhhaann@mail.ru>
2025-02-05 22:15:09 +0500

    тим контроллер аштык, тимвиьюмодел аштык сосын болек тим папка сонын ышыне индекс ашып тимареанын ышындегы адамга берып шыктык мандерын

---
+++ HotelATR.RealPortal0/Controllers/TeamController.cs
@@ -0,0 +1,28 @@
+﻿using HotelATR.RealPortal0.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelATR.RealPortal0.Controllers
+{
+    public class TeamController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<TeamViewModel> teams = new List<TeamViewModel>();
+
+            teams.Add(new TeamViewModel()
+
+            {
+                ImagePath = "img/team/1.jpg",
+                FullName = "Kathy Luis",
+                Position ="( Officer )",
+                Description = "Lorem ipsupm dolor sit amet, conse ctetur adipisicing elit, sed do eiumthgtipsupm dolor sit amet conse",
+                TeamLinks = new List<TeamLinks>()
+                {
+                    new TeamLinks(){ URL="https://www.facebook.com/", LinkType="zmdi-facevook"}
+                }
+            });
+
+            return View(teams);
+        }
+    }
+}
---
+++ HotelATR.RealPortal0/Models/TeamViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelATR.RealPortal0.Models
+{
+    public class TeamViewModel
+    {
+        public string ImagePath { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public string Description { get; set; }
+        public List<TeamLinks> TeamLinks { get; set; }
+    }
+    public class TeamLinks
+    {
+        public string URL { get; set; }
+        public string LinkType { get; set; }
+    }
+}
